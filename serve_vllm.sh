adad2216c7dcbb527b291c0bf349455483351e14
aowen14 <aowen14@gmail.com>
2024-04-17 13:43:52 -0700

    random

---
+++ serve_vllm.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ -z "$VLLM_ARGS" ]
+then
+    if [ ! -z "$MODEL_CMD" ]
+    then
+        VLLM_ARGS="$MODEL_CMD"  
+    else
+        VLLM_ARGS="--model-id TheBloke/Llama-2-7B-chat-GPTQ --quantize gptq"
+    fi
+fi
+
+echo "using args: $VLLM_ARGS" | tee -a /root/debug.log
+
+MODEL_LAUNCH_CMD="vllm.entrypoints.openai.api_server"
+MODEL_PID=$(ps aux | grep "$MODEL_LAUNCH_CMD" | grep -v grep | awk '{print $2}')
+
+if [ -z "$MODEL_PID" ]
+then
+    echo "starting model download" 
+    echo $(ls ..)
+    python3 -m vllm.entrypoints.openai.api_server $VLLM_ARGS
+    echo "launched model" | tee -a /root/debug.log
+else
+    echo "model already running" | tee -a /root/debug.log
+fi
